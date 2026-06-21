e757041c0388f270d4384f08f8954b76a8452d0d
だ <system322940.discord@outlook.jp>
2026-06-21 18:58:32 +0900

    Update workers.js

--- api/api5/workers.js
+++ api/api5/workers.js
@@ -4,7 +4,11 @@ export default {
     const targetUrl = url.searchParams.get('url');
 
     try {
-      const targetResponse = await fetch(targetUrl);
+      const targetResponse = await fetch(targetUrl, {
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+        }
+      });
       const data = await targetResponse.text();
       return new Response(data, { headers: { 'Content-Type': 'text/plain; charset=utf-8' } });
     } catch (error) {
